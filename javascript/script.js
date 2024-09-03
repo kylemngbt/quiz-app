4d70ea2ace1ed1435eda894a84159efed050956e
Kyle Soliveres <kyle.mngbt@gmail.com>
2024-09-03 20:08:48 +0800

    fix: add the e parameter on selectAnswer function

--- javascript/script.js
+++ javascript/script.js
@@ -51,7 +51,7 @@ function resetState() {
   }
 }
 
-function selectAnswer() {
+function selectAnswer(e) {
   const selectedBtn = e.target;
   const isCorrect = selectedBtn.dataset.correct === "true";
   if (isCorrect) {
